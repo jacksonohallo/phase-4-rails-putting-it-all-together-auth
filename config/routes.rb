640e0e8bfa701fee71a102407896221581f84dec
jacksonohallo <jacksonohallo@gmail.com>
2022-10-07 13:48:04 +0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,14 @@ post '/signup', to: 'users#create'
 #GET /me
 get '/me', to: 'users#show'
 
-
 #get/index
-
 get '/users',  to: 'users#index'
+
+
+#POST /login
+post '/login', to: 'sessions#create'
+
+#DELETE /logout
+
+delete '/logout' to: 'sessions#destroy'
 end
